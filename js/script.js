19d0d6c6e4be3dc25d1cf82e19f5630de2ce57c9
Peter Matthews <pjdmatthews7@gmail.com>
2017-11-19 17:35:05 -0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So you want to live at ' + address + '?')
+
+    var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
+
+    $body.append('<img class="bgimg" src="'+ streetViewUrl + '">');
+
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
