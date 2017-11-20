6ecb70ff8cdbc4400d64796c3d7acf6d8f76c3dc
Peter Matthews <pjdmatthews7@gmail.com>
2017-11-20 05:45:20 -0500

    Adding NY Times API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var mykey = config.NY_KEY;
 
 function loadData() {
 
@@ -18,10 +19,29 @@ function loadData() {
 
     $greeting.text('So you want to live at ' + address + '?')
 
-    var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address+'';
+    var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
 
-    $body.append('<img class="bgimg" src="'+ streetViewUrl + '">');
+    $body.append('<img class="bgimg" src="' + streetViewUrl + '">');
 
+    // Built by LucyBot. www.lucybot.com
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': mykey,
+        'q': "Thanksgiving",
+        'fl': "snippet, web_url"
+    });
+    // $.ajax({
+    //     url: url,
+    //     method: 'GET',
+    // }).done(function(result) {
+    //     console.log(result);
+    // }).fail(function(err) {
+    //     throw err;
+    // });
+
+    $.getJSON(url, function(data) {
+        console.log(data);
+    });
 
     return false;
 };
